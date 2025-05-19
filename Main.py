e9a62288fdc2988b62e9d9e1bbb0b42e05830ba8
Dominik Banach <65920764+DominikBanach@users.noreply.github.com>
2025-05-19 14:45:52 +0200

    release 1.0

--- Main.py
+++ Main.py
@@ -2,17 +2,14 @@ from InputParser import InputParser
 from KruskalMSTBuilder import KruskalMSTBuilder
 from Visualizer import Visualizer
 
-NUMBER_OF_EXAMPLES = 8
-PATHS = [f"inputExamples/inputExample{i}.txt" for i in range(NUMBER_OF_EXAMPLES)]
+PATH = "input.txt"
 
 if __name__ == "__main__":
 
-    for PATH in PATHS:
+    parser = InputParser()
+    n, sortedEdges = parser.loadNumberOfVerticesAndSortedEdgesList(PATH)
 
-        parser = InputParser()
-        n, sortedEdges = parser.loadNumberOfVerticesAndSortedEdgesList(PATH)
+    kruskalMSTBuilder = KruskalMSTBuilder(n, sortedEdges)
 
-        kruskalMSTBuilder = KruskalMSTBuilder(n, sortedEdges)
-
-        visualizer = Visualizer(sortedEdges, kruskalMSTBuilder.kruskalMST(), n)
-        visualizer.present()
+    visualizer = Visualizer(sortedEdges, kruskalMSTBuilder.kruskalMST(), n)
+    visualizer.present()
